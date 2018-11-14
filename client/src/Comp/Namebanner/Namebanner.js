c15e6466c87a4adfbacbd4dd06dc55d181e2282b
Ronald Deguzman <bootcampcoding.ronnie@gmail.com>
2018-11-14 10:15:33 -0800

    added display:grid and grid-template:columns:repeat(1,1fr) to the index.css helping the web page to be responsive on mobile devices, still need to make footer component responsive

--- client/src/Comp/Namebanner/Namebanner.js
+++ client/src/Comp/Namebanner/Namebanner.js
@@ -5,10 +5,20 @@ import "./Namebanner.css"
 class Namebanner extends Component {
     render() {
         return (
+            <div className="name-banner">
+                <div className="n-1">
+                </div>
 
-            <h1 className="portfolio-hero-header-h1" id="jumbotron-name">
-                My Name Is Ronaldo
-        </h1>
+                <div className="n-2">
+                    <h1 className="portfolio-hero-header-h1" id="jumbotron-name">
+                    My Name Is Ronaldo
+                    </h1>
+                </div>
+
+                <div className="n-3">
+                </div>
+
+            </div>
 
         )
     }
